fbe37fbbd5abd1295d46d9791edc27d1387e84cd
hecktors <tohe.webdev@gmail.com>
2021-05-29 13:20:58 +0200

    Add basic api error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,21 +6,28 @@ import yelp from "../api/yelp"
 const SearchScreen = () => {
   const [term, setTerm] = useState("")
   const [restaurants, setRestaurants] = useState([])
+  const [errorMsg, setErrorMsg] = useState("")
 
   const searchAPI = async () => {
-    const response = await yelp("search", {
-      params: {
-        term,
-        location: "NYC",
-        limit: 50,
-      },
-    })
-    setRestaurants(response.data.businesses)
+    try {
+      const response = await yelp("search", {
+        params: {
+          term,
+          location: "NYC",
+          limit: 50,
+        },
+      })
+      setRestaurants(response.data.businesses)
+      setErrorMsg("")
+    } catch (err) {
+      setErrorMsg("Something went wrong...")
+    }
   }
 
   return (
     <View>
       <SearchBar term={term} onChange={(newTerm) => setTerm(newTerm)} onSubmit={searchAPI} />
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
       <Text>We have found {restaurants.length} restaurants</Text>
     </View>
   )
